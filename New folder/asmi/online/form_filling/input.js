2fadd924a9c5b7bf31850964963195f0a116f1a5
nandan-23 <86429690+nandan-23@users.noreply.github.com>
2023-07-29 02:28:28 +0530

    main

--- New folder/asmi/online/form_filling/input.js
+++ New folder/asmi/online/form_filling/input.js
@@ -14,3 +14,30 @@ document.getElementById('destination').addEventListener('change', function(){
     })
     .catch(error => console.log(error));
 });
+
+
+
+
+
+document.getElementById('destination').addEventListener('change', function(){
+    let place = this.options[this.selectedIndex].text;
+    
+    // Add zoomOut class
+    document.getElementById('destinationImage').classList.add('zoomOut');
+    
+    fetch(`https://api.unsplash.com/search/photos?query=${place}&client_id=${unsplashAccessKey}`)
+    .then(response => response.json())
+    .then(data => {
+        if(data && data.results && data.results.length > 0) {
+            let imageUrl = data.results[0].urls.regular;
+            // Set timeout to change the image after a delay, during which the image will be scaled to 0
+            setTimeout(() => {
+                document.getElementById('destinationImage').src = imageUrl;
+                // Remove zoomOut class
+                document.getElementById('destinationImage').classList.remove('zoomOut');
+            }, 1000); // Delay time should be the same as the transition duration
+        }
+    })
+    .catch(error => console.log(error));
+});
+
