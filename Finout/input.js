665e32d1b279744abcf4ac858c55f9fd5086a00d
nandan-23 <86429690+nandan-23@users.noreply.github.com>
2023-08-03 23:30:47 +0530

    Add files via upload

---
+++ Finout/input.js
@@ -0,0 +1,75 @@
+// Replace 'YOUR_ACCESS_KEY' with your actual Unsplash Access Key
+let unsplashAccessKey = 'T6dRWHW6uAnjkjScxkYjojWO_Cy3UfYM7YP-HIuuuHA'; 
+
+document.getElementById('destination').addEventListener('change', function(){
+    let place = this.options[this.selectedIndex].text;
+    
+    fetch(`https://api.unsplash.com/search/photos?query=${place}&client_id=${unsplashAccessKey}`)
+    .then(response => response.json())
+    .then(data => {
+        if(data && data.results && data.results.length > 0) {
+            let imageUrl = data.results[0].urls.regular;
+            document.getElementById('destinationImage').src = imageUrl;
+        }
+    })
+    .catch(error => console.log(error));
+});
+
+
+
+
+
+document.getElementById('destination').addEventListener('change', function(){
+    let place = this.options[this.selectedIndex].text;
+    
+    // Add zoomOut class
+    document.getElementById('destinationImage').classList.add('zoomOut');
+    
+    fetch(`https://api.unsplash.com/search/photos?query=${place}&client_id=${unsplashAccessKey}`)
+    .then(response => response.json())
+    .then(data => {
+        if(data && data.results && data.results.length > 0) {
+            let imageUrl = data.results[0].urls.regular;
+            // Set timeout to change the image after a delay, during which the image will be scaled to 0
+            setTimeout(() => {
+                document.getElementById('destinationImage').src = imageUrl;
+                // Remove zoomOut class
+                document.getElementById('destinationImage').classList.remove('zoomOut');
+            }, 1000); // Delay time should be the same as the transition duration
+        }
+    })
+    .catch(error => console.log(error));
+});
+
+
+document.getElementById('btn1').addEventListener('click', function(e) {
+    e.preventDefault();
+  
+    let changingText = document.getElementById('changingText');
+    changingText.style.animation = "fadeout 5s";
+    
+    // Assuming this is where you get the response
+    new Promise((resolve) => {
+      setTimeout(() => {
+        changingText.textContent = "Let's Travel";
+        resolve();
+      }, 1000);
+    }).then(() => {
+      changingText.style.animation = "none";
+      changingText.getBoundingClientRect(); // Triggers reflow
+      changingText.style.animation = "fadein 2s";
+      
+      // Show the card with the response
+      let responseCard = document.getElementById('responseCard');
+      responseCard.style.display = "block";
+      
+      // Add content to the card
+      document.getElementById('responseText').textContent = "This is the response";
+  
+      // Show the div and scroll to it
+      document.getElementById('hiddenContent').style.display = "flex";
+      document.body.style.overflow = "auto";
+      window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    });
+  });
+  
